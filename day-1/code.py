6708f626670fb2941a593aef49de8f2a38376aad
peter stickney <peter@peterstickney.com>
2021-12-08 08:17:29 -0500

    day 1 done, mf'ers

--- day-1/code.py
+++ day-1/code.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 
-print("hello, world")
+data_file = open("./input", 'r')
+data = []
+
+# larger measurement counter
+lm = 0
+with open("./input") as input_text:
+    for line in input_text:
+        data.append(int(line.strip()))
+
+num_entries = len(data)
+
+i = 0
+
+while i < num_entries:
+    if i > 0:
+        if data[i] > data[i-1]:
+            lm += 1
+    i += 1
+
+print(lm)
+
