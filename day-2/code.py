7be7df50ee8271ea06516fb1a18998cf3685cd63
peter stickney <peter@peterstickney.com>
2021-12-08 08:33:36 -0500

    day 2 done, mf'ers

---
+++ day-2/code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+data_file = open("./input", 'r')
+data = []
+hpos = 0
+depth = 0
+
+with open("./input") as input_text:
+    for line in input_text:
+        data.append(line.strip())
+
+for cmd in data:
+    split = cmd.split(" ")
+    if split[0] == "forward":
+        hpos += int(split[1])
+    if split[0] == "down":
+        depth += int(split[1])
+    if split[0] == "up":
+        depth -= int(split[1])
+
+print("Your final position match is %d" % (hpos*depth))
