d5b175a3c40e3fcdb09033ebd99b158015a2c799
peter stickney <peter@peterstickney.com>
2021-12-08 22:44:27 -0500

    finished the second parts of these days

--- day-1/code.py
+++ day-1/code.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import math
 
 data_file = open("./input", 'r')
 data = []
@@ -15,9 +16,49 @@ i = 0
 
 while i < num_entries:
     if i > 0:
-        if data[i] > data[i-1]:
+        if data[i] > data[i - 1]:
             lm += 1
     i += 1
 
 print(lm)
+
+
+# Function to check
+# Log base 2
+def Log2(x):
+    return math.log10(x) / math.log10(2)
+
+
+# Function to check
+# if x is power of 2
+def ispoweroftwo(x):
+    return math.ceil(Log2(x)) == math.floor(Log2(x))
+
+
+i = 0
+a = 0
+b = 1
+c = 2
+this_sum = 0
+prev_sum = 0
+num_increases = 0
+
+while i < num_entries:
+    if i == 0:
+        prev_sum = data[a] + data[b] + data[c]
+    i += 1
+    a += 1
+    b += 1
+    c += 1
+    if i <= num_entries - 3:
+        this_sum = data[a] + data[b] + data[c]
+    # do the logic
+        if this_sum > prev_sum:
+            num_increases += 1
+        # move the sum fwd
+        this_sum = prev_sum
+        prev_sum = data[a] + data[b] + data[c]
+
+print(num_increases)
+
 
--- day-2/code.py
+++ day-2/code.py
@@ -2,12 +2,14 @@
 
 data_file = open("./input", 'r')
 data = []
-hpos = 0
-depth = 0
 
 with open("./input") as input_text:
     for line in input_text:
         data.append(line.strip())
+
+# part 1
+hpos = 0
+depth = 0
 
 for cmd in data:
     split = cmd.split(" ")
@@ -18,4 +20,21 @@ for cmd in data:
     if split[0] == "up":
         depth -= int(split[1])
 
-print("Your final position math is %d" % (hpos*depth))
+print("Your final position math for part 1 is %d" % (hpos*depth))
+
+# part 2
+hpos = 0
+depth = 0
+aim = 0
+
+for cmd in data:
+    split = cmd.split(" ")
+    if split[0] == "forward":
+        hpos += int(split[1])
+        depth += aim * int(split[1])
+    if split[0] == "down":
+        aim += int(split[1])
+    if split[0] == "up":
+        aim -= int(split[1])
+
+print("Your final position math for part 2 is %d" % (hpos*depth))
