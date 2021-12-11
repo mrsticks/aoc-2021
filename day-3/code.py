1855a93bdfd1df7cd3f7cfb8322d931adf0da4cf
peter stickney <peter@peterstickney.com>
2021-12-11 18:31:47 -0500

    fuckind around

--- day-3/code.py
+++ day-3/code.py
@@ -3,6 +3,22 @@
 data_file = open("./input", 'r')
 gamma = ""
 epsilon = ""
+zerobits = 0
+onebits = 0
+
+i = 0
+j = 0
+matrix = []
 with open("./input") as input_text:
     for line in input_text:
-        print(line[0])
+        line = line.strip()
+        matrix.append([])
+        matrix[i].append(int(line[j]))
+        j += 1
+    i += 1
+
+#for line in matrix:
+
+print("this is the length of matrix %d" % len(matrix))
+print("this is the length of matrix[1] %d " % len(matrix[1]))
+print(matrix[1])
